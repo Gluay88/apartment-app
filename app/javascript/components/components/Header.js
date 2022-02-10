8487a7471e19470741c6c7b2c292d52a1603730c
gluay88 <p.wantagaew@gmail.com>
2022-02-10 12:08:40 -0800

    login

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
     return (
       <div>
         <h1>Apartment Hunter App</h1>
-        
+        {/* pass the props here.. */}
         <Nav>
           <NavItem>
           <div className="nav-bar">
